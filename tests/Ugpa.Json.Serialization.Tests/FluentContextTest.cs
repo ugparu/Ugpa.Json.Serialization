b83177b56c9cdd093b98b72714de4bb3ffd1cfc1
PleshakovDV <pleshakovdv@ugpa.ru>
2021-05-19 11:02:37 +0300

    Fixed problem with missing SerializationBinder.

---
+++ tests/Ugpa.Json.Serialization.Tests/FluentContextTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Ugpa.Json.Serialization.Tests
+{
+    public sealed class FluentContextTest
+    {
+        [Fact]
+        public void TypeNameBindingIsValid()
+        {
+            var context = new FluentContext();
+            context.Configure<FluentContextTest>(_ => _.HasContractName("test"));
+
+            context.BindToName(typeof(FluentContextTest), out _, out var name);
+            Assert.Equal("test", name);
+
+            var type = context.BindToType(null, "test");
+            Assert.Equal(typeof(FluentContextTest), type);
+        }
+    }
+}
